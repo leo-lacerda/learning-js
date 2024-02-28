c61953763029ccc12237085acecde98cf163c41d
Leo Lacerda <51065340+leo-lacerda@users.noreply.github.com>
2024-02-27 23:45:46 -0300

    Create speed.js

---
+++ speed.js
@@ -0,0 +1,7 @@
+let speed = 60
+console.log(`Your car speed is ${speed}km/h`)
+if (speed > 60) {
+  console.log(`You're above maximum speed! Here's your ticket!`)
+} else {
+  console.log(`You're ok! Have a nice trip!`)
+}
