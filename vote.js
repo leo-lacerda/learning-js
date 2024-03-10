c9b0a4e2d9f735d71d4ef844c108da3d0111e413
Leo Lacerda <51065340+leo-lacerda@users.noreply.github.com>
2024-03-10 19:31:22 -0300

    Create vote.js

---
+++ vote.js
@@ -0,0 +1,11 @@
+let age = 66
+
+if (age < 16) {
+  console.log("You can't vote.")
+} else {
+  if (age < 18 || age >65) {
+    console.log("Do you know you can vote? Go for it!")
+  } else {
+    console.log("Your HAVE to vote!")
+  }
+}
